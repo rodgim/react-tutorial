19ff6678423946d1bf79a59dc2825161ba440cfa
Rodrigo Gimenez <rodrigo05101990@gmail.com>
2021-01-10 16:40:26 -0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>
-                        {dish.name}
-                    </CardTitle>
-                    <CardText>
-                        {dish.description}
-                    </CardText>
-                </CardBody>
-            </Card>
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>
+                            {dish.name}
+                        </CardTitle>
+                        <CardText>
+                            {dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </div>
         );
     }
 
     function RenderComments({comments}){
         if(comments != null){
-            console.log("comment");
             const commentsRender = comments.map((comment) => {
                 return (
-                    <ListGroupItem key={comment.id}>
+                    <li>
                         <p>
                             {comment.comment}
                         </p>
                         <p>
                             -- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         </p>
-                    </ListGroupItem>
+                    </li>
                 );
             });
             return (
-                <div>
+                <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    <ListGroup>
+                    <ul className="list-unstyled">
                         {commentsRender}
-                    </ListGroup>
+                    </ul>
                 </div>
             );
         }else{
@@ -52,13 +54,19 @@ import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem 
             return(
                 <div className="container">
                     <div className="row">
-                    <div className="col-12 col-md-5 m-1">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
                         <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
-                    </div>
-                </div>
                 </div>
             );
         }else{
