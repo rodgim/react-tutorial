cdb1cf89950967f3846ff78bef2b55bdb5835f25
Rodrigo Gimenez <rodrigo05101990@gmail.com>
2021-01-26 17:42:02 -0300

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -22,7 +22,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
         );
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if(comments != null){
             const commentsRender = comments.map((comment) => {
                 return (
@@ -41,7 +41,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
                         {commentsRender}
-                        <CommentForm />
+                        <CommentForm dishId={dishId} addComment={addComment}/>
                     </ul>
                 </div>
             );
@@ -68,7 +68,9 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                     </div>
                     <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments} 
+                            addComment={props.addComment}
+                            dishId={props.dish.id}/>
                     </div>
                 </div>
             );
@@ -100,8 +102,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values){
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values))
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
